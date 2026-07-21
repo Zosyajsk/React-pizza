c85b48cefbd41339e6006dfe2503a6ebee90c67c
zazholdas <zosyajsk@mail.ru>
2026-07-21 18:11:24 +0500

    feat: fetching data from mockapi

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import React from "react";
-
 import "./scss/app.scss";
-
-import pizzas from "./assets/pizzas.json";
-
 import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
 
-console.log(pizzas);
-
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://6a5883ab914a025dcff41fc0.mockapi.io/items")
+      .then((res) => {
+        return res.json();
+      })
+      .then((arr) => {
+        setItems(arr);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
           <div className="content__top">
-            {/* <Categories /> */}
+            {<Categories />}
             <Sort />
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj) => (
+            {items.map((obj) => (
               <PizzaBlock
                 key={obj.id}
                 title={obj.title}
